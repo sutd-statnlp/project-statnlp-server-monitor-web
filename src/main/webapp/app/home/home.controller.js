95828f4ec32bb764111848a34ded55065357fe25
Ai Nguyen <ainguyenkaka@gmail.com>
2018-02-07 17:22:07 +0800

    remain autorefresh

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -63,9 +63,6 @@
                 }
             };
 
-            vm.isAutoRefresh = false;
-            refreshInterval = null;
-
             cpuChart = {
                 plot: null,
                 data: []
