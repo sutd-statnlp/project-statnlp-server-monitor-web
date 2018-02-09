a9122dcb6cfa1616856774694ee46ac161b2940d
Ai Nguyen <ainguyenkaka@gmail.com>
2018-02-09 13:30:23 +0800

    adjust icons

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('statnlpApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1518146708189)
+        .constant('BUILD_TIMESTAMP', 1518154180747)
 ;
 })();
